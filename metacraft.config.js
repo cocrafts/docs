e6feffd5834e5ca8266cb35bda52ad87d0bd07bb
kayaba <kayaba.fluctlight@gmail.com>
2022-09-12 12:22:30 +0700

    optimize local package size

--- metacraft.config.js
+++ metacraft.config.js
@@ -1,3 +1,5 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 const setEnvironments = (configs, internal) => {
 	const { webpack } = internal.modules;
 	const { DefinePlugin } = webpack;
@@ -13,10 +15,32 @@ const setEnvironments = (configs, internal) => {
 	return configs;
 };
 
+const externals = (configs) => {
+	// configs.plugins.push(new BundleAnalyzerPlugin());
+
+	configs.externals = {
+		react: 'React',
+		lodash: '_',
+		'react-dom': 'ReactDOM',
+		'react-art': 'ReactART',
+	};
+
+	return configs;
+};
+
+const customLoaders = (configs) => {
+	configs.module.rules.push({
+		test: /\.md$/i,
+		type: 'asset/source',
+	});
+
+	return configs;
+};
+
 module.exports = {
 	useBabel: true,
 	buildId: () => 'app',
-	webpackMiddlewares: [setEnvironments],
+	webpackMiddlewares: [setEnvironments, externals, customLoaders],
 	moduleAlias: {
 		global: {
 			'react-native': 'react-native-web',
