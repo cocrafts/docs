ddf15172e5a7ff9405ba52437081bee903ec8f6a
kayaba <kayaba.fluctlight@gmail.com>
2022-09-14 12:51:11 +0700

    favicon for docs

--- metacraft.config.js
+++ metacraft.config.js
@@ -1,3 +1,5 @@
+const { resolve } = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const setEnvironments = (configs, internal) => {
@@ -11,6 +13,27 @@ const setEnvironments = (configs, internal) => {
 		__DEV__: !isProduction,
 		ENV: JSON.stringify(env),
 	});
+
+	return configs;
+};
+
+const copyAssets = (configs) => {
+	configs.plugins.push(
+		new CopyPlugin({
+			patterns: [
+				{
+					from: resolve(process.cwd(), 'assets/'),
+					to: './',
+					filter: (uri) => {
+						const isFont = uri.indexOf('/fonts/') >= 0;
+						const isTemplate = uri.endsWith('.ejs') || uri.endsWith('.sass');
+
+						return !isFont && !isTemplate;
+					},
+				},
+			],
+		}),
+	);
 
 	return configs;
 };
@@ -40,7 +63,7 @@ const customLoaders = (configs) => {
 module.exports = {
 	useBabel: true,
 	buildId: () => 'app',
-	webpackMiddlewares: [setEnvironments, externals, customLoaders],
+	webpackMiddlewares: [setEnvironments, copyAssets, externals, customLoaders],
 	moduleAlias: {
 		global: {
 			'react-native': 'react-native-web',
